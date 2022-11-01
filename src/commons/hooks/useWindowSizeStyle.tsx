6a6607ad40a9d930e17c69b738a2d79a5a66b8d4
jaein0304 <ddc0406@naver.com>
2022-11-01 16:33:04 +0900

    [Feat] custom hook 윈도우사이즈스타일 추가

---
+++ src/commons/hooks/useWindowSizeStyle.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function useWindowSizeStyle() {
+  const [windowSize, setWindowSize] = useState(false);
+
+  // 윈도우 사이즈가 991보다(태블릿사이즈) 작으면 윈도우사이즈 true
+  const handleResize = () => {
+    if (window.innerWidth <= 991) setWindowSize(true);
+    else setWindowSize(false);
+  };
+
+  useEffect(() => {
+    if (window.innerWidth <= 991) setWindowSize(true);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowSize]); // 윈도우 사이즈가 변할 때 rerender
+
+  return { handleResize, windowSize };
+}
