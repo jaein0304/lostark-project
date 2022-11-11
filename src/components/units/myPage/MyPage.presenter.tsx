0bb42bba815be785d67313c561e978c0666f23ea
jaein0304 <ddc0406@naver.com>
2022-11-11 14:39:56 +0900

    [refactor] 마이페이지 네비게이션 공통으로 묶기

--- src/components/units/myPage/MyPage.presenter.tsx
+++ src/components/units/myPage/MyPage.presenter.tsx
@@ -1,26 +1,5 @@
-import * as S from "./MyPage.styles";
-import MyPageTab from "./myPageTab/MyPageTab.container";
+import MyPageNav from "./myPageNav/MyPageNav";
 
 export default function MyPageUI() {
-  return (
-    <S.Wrapper>
-      <S.InfoWrapper>
-        <S.Info>
-          <S.Row>
-            <S.Profile src="https://i.esdrop.com/d/f/eJqYA2AGQd/fsZ8eanTJx.png" />
-            <S.Col>
-              <S.Name>편히잠드소서리스</S.Name>
-              <S.ChangePW>비밀번호 변경</S.ChangePW>
-            </S.Col>
-            <S.PointWrapper>
-              <S.Crystal />
-              <S.Point>5000</S.Point>
-              <S.ChargeBtn>충전</S.ChargeBtn>
-            </S.PointWrapper>
-          </S.Row>
-        </S.Info>
-      </S.InfoWrapper>
-      <MyPageTab />
-    </S.Wrapper>
-  );
+  return <MyPageNav />;
 }
